274098f673a48feaf2797d8fc72a6efb5dd90d58
orhfq4 <orhfq4@umsystem.edu>
2024-11-22 23:37:57 -0600

    Adding initial create for webpages

---
+++ owens-website/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col pt-16">
+      <div className="container mx-auto mt-4 flex-grow">
+        <h1 className="text-2xl font-bold mb-4">Home</h1>
+        <p className="text-lg">Welcome to the homepage!</p>
+      </div>
+    </div>
+  );
+}
